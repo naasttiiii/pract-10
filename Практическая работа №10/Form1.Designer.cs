ba6abf99866c0d2afa4ccf8c8ff67ae5497a440f
исп-31 <-31@LAB17-02>
2021-03-30 13:24:14 +0300

    Добавьте файлы проекта.

---
+++ Практическая работа №10/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace Практическая_работа__10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnFill = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnAnswer = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtKolList2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtKolList1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.спискиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.заполнитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.проверитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(12, 31);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(163, 328);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 18;
+            this.listBox2.Location = new System.Drawing.Point(179, 31);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(163, 328);
+            this.listBox2.TabIndex = 1;
+            // 
+            // btnFill
+            // 
+            this.btnFill.Location = new System.Drawing.Point(1, 98);
+            this.btnFill.Name = "btnFill";
+            this.btnFill.Size = new System.Drawing.Size(107, 34);
+            this.btnFill.TabIndex = 3;
+            this.btnFill.Text = "Заполнить";
+            this.btnFill.UseVisualStyleBackColor = true;
+            this.btnFill.Click += new System.EventHandler(this.btnFill_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(462, 325);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(112, 34);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Location = new System.Drawing.Point(348, 325);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(113, 34);
+            this.btnInfo.TabIndex = 5;
+            this.btnInfo.Text = "О программе";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            this.btnInfo.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // btnAnswer
+            // 
+            this.btnAnswer.Location = new System.Drawing.Point(51, 98);
+            this.btnAnswer.Name = "btnAnswer";
+            this.btnAnswer.Size = new System.Drawing.Size(123, 34);
+            this.btnAnswer.TabIndex = 6;
+            this.btnAnswer.Text = "Проверить";
+            this.btnAnswer.UseVisualStyleBackColor = true;
+            this.btnAnswer.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.txtKolList2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtKolList1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnFill);
+            this.groupBox1.Location = new System.Drawing.Point(348, 31);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(224, 144);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Управление списками";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(115, 98);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(107, 34);
+            this.btnClear.TabIndex = 4;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtKolList2
+            // 
+            this.txtKolList2.Location = new System.Drawing.Point(118, 68);
+            this.txtKolList2.Name = "txtKolList2";
+            this.txtKolList2.Size = new System.Drawing.Size(100, 24);
+            this.txtKolList2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(112, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Кол-во чисел \r\n     2 списка";
+            // 
+            // txtKolList1
+            // 
+            this.txtKolList1.Location = new System.Drawing.Point(6, 68);
+            this.txtKolList1.Name = "txtKolList1";
+            this.txtKolList1.Size = new System.Drawing.Size(100, 24);
+            this.txtKolList1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Кол-во чисел \r\n     1 списка";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtAnswer);
+            this.groupBox2.Controls.Add(this.btnAnswer);
+            this.groupBox2.Location = new System.Drawing.Point(348, 181);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(224, 138);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Решение";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(6, 23);
+            this.txtAnswer.Multiline = true;
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.ReadOnly = true;
+            this.txtAnswer.Size = new System.Drawing.Size(212, 69);
+            this.txtAnswer.TabIndex = 7;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.спискиToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(582, 28);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // спискиToolStripMenuItem
+            // 
+            this.спискиToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.заполнитьToolStripMenuItem,
+            this.проверитьToolStripMenuItem,
+            this.очиститьToolStripMenuItem});
+            this.спискиToolStripMenuItem.Name = "спискиToolStripMenuItem";
+            this.спискиToolStripMenuItem.Size = new System.Drawing.Size(71, 24);
+            this.спискиToolStripMenuItem.Text = "Списки";
+            // 
+            // заполнитьToolStripMenuItem
+            // 
+            this.заполнитьToolStripMenuItem.Name = "заполнитьToolStripMenuItem";
+            this.заполнитьToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.заполнитьToolStripMenuItem.Text = "Заполнить";
+            this.заполнитьToolStripMenuItem.Click += new System.EventHandler(this.btnFill_Click);
+            // 
+            // проверитьToolStripMenuItem
+            // 
+            this.проверитьToolStripMenuItem.Name = "проверитьToolStripMenuItem";
+            this.проверитьToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.проверитьToolStripMenuItem.Text = "Проверить";
+            this.проверитьToolStripMenuItem.Click += new System.EventHandler(this.btnAnswer_Click);
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(180, 24);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(116, 24);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.btnInfo_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 369);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Практическая работа №10";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnFill;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnAnswer;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtKolList2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtKolList1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem спискиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem заполнитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem проверитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+    }
+}
+
---
+++ Практическая работа №10/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практическая_работа__10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Использовать коллекцию для выполнения задания. Оформить модули комментариями.
+        //Заданы два массива. Проверить, сколько элементов первого массива превосходят соответствующие элементы второго массива. 
+
+        ArrayList array1 = new ArrayList();
+        ArrayList array2 = new ArrayList();
+
+        private void btnFill_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            // Получаем количество значений
+            int kol1 = Convert.ToInt32(txtKolList1.Text);
+            int kol2 = Convert.ToInt32(txtKolList2.Text);
+
+            // Заполняем список случайными значениями
+            for (int i = 0; i < kol1; i++)
+            {
+                array1.Add(rnd.Next(-10, 10));
+            }
+            listBox1.Items.AddRange(array1.ToArray());
+
+
+            for (int i = 0; i < kol2; i++)
+            {
+                array2.Add(rnd.Next(-10, 10));
+            }
+            listBox2.Items.AddRange(array2.ToArray());
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            array1.Clear();
+            array2.Clear();
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+        }
+
+        private void btnAnswer_Click(object sender, EventArgs e)
+        {
+            int kol = 0;
+            for (int i = 0; i < array1.Count; i++)
+                if ((int)array1[i] > (int)array2[i])
+                {
+                    kol++;
+                    txtAnswer.Text = Convert.ToString(kol) + " " + "элемента(ов) первого массива превосходят соответствующие элементы второго массива.";
+                }
+                else
+                {
+                    txtAnswer.Text = "Элементы первого массива не превосходят соответствующие элементы второго массива.";
+                }
+          
+        }
+
+        private void btnInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Хужанова Анастасия\n" +
+                "Вариант 10\n" +
+                "Заданы два массива.\n" +
+                "Проверить, сколько элементов первого массива превосходят соответствующие элементы второго массива.", "О программе", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+
+
+
+}
